0888b0f4f4401ef8208c2539009b06dfbc5fe9f1
Agustin Varas <Agustin.Varas@alumnos.frgp.utn.edu.ar>
2024-12-14 15:39:00 +0000

    feat: Installed mongoose and mongodb dependencies, added variable mongoose and stablished connection to DB

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI,{ 
+  useNewUrlParser: true, useUndefinedTopology: true
+});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
