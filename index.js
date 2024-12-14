57bc32945665fafe095bc315f6713c63e9041a7a
Agustin Varas <Agustin.Varas@alumnos.frgp.utn.edu.ar>
2024-12-14 16:00:48 +0000

    feat: Added db connection verification

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI,{ 
   useNewUrlParser: true, useUndefinedTopology: true
-});
+})
+  .then(() => console.log('MongoDB connected succesfully'))
+  .catch((err) => console.error('Error connecting to DB: ' + err));
 
 //Schema
 const UrlSchema = new mongoose.Schema({
