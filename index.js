6737e3f2e0003a60c7dc0d67bc1acb97bd6f42f8
Agustin Varas <Agustin.Varas@alumnos.frgp.utn.edu.ar>
2024-12-14 16:15:31 +0000

    feat: Created post function to handle /api/shorturl url

--- index.js
+++ index.js
@@ -45,7 +45,9 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-
+app.post('/api/shorturl', (req, res, next) => {
+  
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
